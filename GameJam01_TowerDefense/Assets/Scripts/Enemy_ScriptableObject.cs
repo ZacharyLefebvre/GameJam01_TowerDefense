1b2f124d02dfbc263d10ca16256c012134895e8f
ZacharyLefebvre <zacharylefebvre@hotmail.com>
2019-09-16 21:46:52 +0200

    Setting up a few scripts start idea

---
+++ GameJam01_TowerDefense/Assets/Scripts/Enemy_ScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class Enemy_ScriptableObject : ScriptableObject
+{
+    public new string name;
+
+    public int health;
+    public int damage;
+    public int speed;
+
+    public bool canFly;
+}
---
+++ GameJam01_TowerDefense/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance { get; private set; }
+
+    public int playerHealth = 100;
+
+	void Awake ()
+    {
+        Instance = this;
+	}
+
+    public void DamagePlayer()
+    {
+
+    }
+}
